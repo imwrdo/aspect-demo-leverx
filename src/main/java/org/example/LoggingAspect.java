aa4bf48cd0a53aac5bb9464d3772628a4f0d5627
imwrdo <sheinnickolas@gmail.com>
2025-02-25 21:25:18 +0100

    fix logging aspect

--- src/main/java/org/example/LoggingAspect.java
+++ src/main/java/org/example/LoggingAspect.java
@@ -2,8 +2,10 @@ package org.example;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class LoggingAspect {
     @Before("execution(* org.example.ShoppingCard.checkout())")
     public void logger(){
