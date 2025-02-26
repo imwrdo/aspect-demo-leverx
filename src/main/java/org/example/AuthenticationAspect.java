a5e9f70b571e81ef89e321b2ba762862eb326af7
imwrdo <sheinnickolas@gmail.com>
2025-02-26 11:52:56 +0100

    feat: add more functionality

--- src/main/java/org/example/AuthenticationAspect.java
+++ src/main/java/org/example/AuthenticationAspect.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.aspectj.lang.JoinPoint;
+
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
